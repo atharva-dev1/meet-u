6cd51d910eaaa17d4284877394d160af306a8039
atharva-dev1 <2431560atharva@gmail.com>
2026-07-14 13:28:52 +0530

    some important changes

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "coffee",
+  theme: localStorage.getItem("meetu-theme") || "coffee",
   setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
+    localStorage.setItem("meetu-theme", theme);
     set({ theme });
   },
 }));
